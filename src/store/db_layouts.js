565a7cecc3df365feff64f71cbf1ff6b19b9e093
Juan Abadia <yabadabu@gmail.com>
2016-01-29 22:24:13 +0100

    Layout proformas updated

---
+++ src/store/db_layouts.js
@@ -0,0 +1,40 @@
+const all_layouts = {
+  proforma: {
+    class_name: "proforma",
+    title: "Recambios - Proformas",
+    table: "[Recambios - Proformas]",
+    key_field: 'IDProforma',
+    search: {
+      fuzzy: {
+        fields: [
+          { field:"IDProforma", style:{width:"10%"}, filter:"IDProforma like '%__FIELD__%'" },
+          { field:"Empresa", style:{width:"70%"}, filter:"Empresa like '%__FIELD__%'" }
+        ],
+        title: 'Número de la Proforma',
+        min_num_chars: 3      // to fire the trigger
+      },
+      exact: { 
+        cmd: "Recambios.Proforma.ByID",
+        filter: "IDProforma = '__FIELD__'" 
+      }
+    },
+    fields: [
+      { field:"Search", type:"action", hint:"Buscar una empresa" },
+      { field:"IDProforma", type:"text", hint:"# Proforma", read_only:true },
+      { field:"Empresa", type:"text", hint:"Nombre de la empresa", style:{ width:"50%"} },
+      { field:"NIF", type:"text", hint:"NIF/CIF", style:{ width:"10%"} },
+      { field:"Fecha", type:"date", mode:"landscape", hint:"Fecha de Creación", textstyle:{ width:"80px" }, style:{display:"inline-block" } },
+      { type:"separator"} ,
+      { field:"Calle", type:"text", style:{ width:"30%" } },
+      { field:"Poblacion", type:"text", style:{ width:"30%" } },
+      { field:"CP", type:"text", hint:"CP", style:{ width:"10%" } },
+      { field:"Provincia", type:"provincia", hint:"Provincia", style:{ width:"15%" } },
+      { type:"separator"},
+      { field:"Notas", type:"text", multiLine:true, hint:"Notas adicionales", fullWidth:true }
+    ]
+  }
+};
+
+export default function getLayout( name ) {
+  return all_layouts[name];
+}
